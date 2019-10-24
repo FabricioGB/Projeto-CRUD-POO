d6b47bc1c4e608d826cc261a10eb51d1f1bc8895
Fabricio <gondimbritojbe@gmail.com>
2019-10-23 22:57:18 -0200

    Primeiro CRUD

--- ProjetoCRUD/src/univs/edu/usuario/UsuarioDAO.java
+++ ProjetoCRUD/src/univs/edu/usuario/UsuarioDAO.java
@@ -3,6 +3,7 @@ package univs.edu.usuario;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import univs.edu.util.HibernateUtil;
 
 public class UsuarioDAO {
@@ -15,5 +16,26 @@ public class UsuarioDAO {
         sessao.save(usuario);
         transacao.commit();
         sessao.close();
+    }
+    public void excluir(Usuario usuario){
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        sessao.delete(usuario);
+        transacao.commit();
+        sessao.close();
+    }
+   public void editar(Usuario usuario){
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        sessao.update(usuario);
+        transacao.commit();
+        sessao.close();
+    }
+   public Usuario pesquisar(int id ){
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        Usuario usuario = (Usuario) sessao.createCriteria(Usuario.class).add(Restrictions.eq("id", id)).uniqueResult();
+        sessao.close();
+        return usuario;
     }
 }
